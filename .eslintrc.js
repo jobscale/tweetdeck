6bb8ed50c90181e377e2a0c2f085bab78e036fce
jobscale <jobscale@jsx.jp>
2018-10-05 18:53:35 +0900

    tweetdeck

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// https://eslint.org/docs/user-guide/configuring
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html',
+  ],
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build/webpack.base.conf.js',
+      },
+    },
+  },
+  // add your custom rules here
+  rules: {
+    indent: ['error', 2, { MemberExpression: 0 }],
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never',
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'arrow-parens': 'off',
+    'no-return-assign': 'off',
+    'no-plusplus': 'off',
+    'no-confusing-arrow': 'off',
+    'class-methods-use-this': 'off',
+    'no-await-in-loop': 'off',
+    'import/prefer-default-export': 'off',
+    'no-bitwise': ['error', { 'int32Hint': true }],
+  },
+};
--- app/index.js
+++ app/index.js
@@ -1,50 +1,45 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-
+const { app, BrowserWindow, Tray } = require('electron');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () {
+let win;
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
+  win = new BrowserWindow({ width: 1200, height: 1600, icon: 'app/favicon.ico' });
+  const tray = new Tray('app/icon.png');
+  tray.on('click', () => win.isVisible() ? win.hide() : win.show());
+  win.on('show', () => tray.setHighlightMode('always'));
+  win.on('hide', () => tray.setHighlightMode('never'));
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
+  win.loadFile('app/index.html');
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
-
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    win = null;
+  });
 }
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
+app.on('ready', createWindow);
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
-
-app.on('activate', function () {
+});
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
+  if (win === null) {
+    createWindow();
   }
-})
-
+});
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
