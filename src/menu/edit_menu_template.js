dba16f9d31e7bd3d70e185f93909d97b16a57f8f
jobscale <jobscale@jsx.jp>
2018-10-09 18:13:43 +0900

    add menu

--- src/menu/edit_menu_template.js
+++ src/menu/edit_menu_template.js
@@ -1,4 +1,4 @@
-export const editMenuTemplate = {
+export const menuTemplates = [{
   label: 'Edit',
   submenu: [
     { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
@@ -9,4 +9,40 @@ export const editMenuTemplate = {
     { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
     { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
   ],
-};
+}, {
+  label: 'Color',
+  submenu: [{
+    label: 'Dark',
+    accelerator: 'CmdOrCtrl+D',
+    click() {},
+  }, {
+    label: 'Light',
+    accelerator: 'CmdOrCtrl+L',
+    click() {},
+  }],
+}, {
+  label: 'View',
+  submenu: [{
+    label: 'Reload',
+    accelerator: 'CmdOrCtrl+R',
+    click(item, focusWindow) {
+      if (!focusWindow) return;
+      focusWindow.reload();
+    },
+  }, {
+    label: 'Toggle Developer Tools',
+    accelerator: (process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I'),
+    click(item, focusWindow) {
+      if (!focusWindow) return;
+      focusWindow.webContents.toggleDevTools();
+    },
+  }, {
+    type: 'separator',
+  }, {
+    role: 'resetzoom',
+  }, {
+    role: 'zoomin',
+  }, {
+    role: 'zoomout',
+  }],
+}];
--- src/renderer.js
+++ src/renderer.js
@@ -6,7 +6,7 @@
 import path from 'path';
 import url from 'url';
 import { app, Menu } from 'electron';
-import { editMenuTemplate } from './menu/edit_menu_template';
+import { menuTemplates } from './menu/edit_menu_template';
 import { devMenuTemplate } from './menu/dev_menu_template';
 import createWindow from './helpers/window';
 
@@ -14,13 +14,86 @@ import createWindow from './helpers/window';
 // in config/env_xxx.json file.
 import { env } from './config';
 
-const setApplicationMenu = () => {
-  const menus = [editMenuTemplate];
-  if (env.name !== 'production') {
-    menus.push(devMenuTemplate);
+class Daemon {
+  constructor() {
+    setTimeout(() => this.first(), 5000);
   }
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
-};
+
+  first() {
+    const cmd = `
+    document.head.appendChild(document.createElement("style"))
+    .innerText = "html{background-color:black;color:white;}body{opacity:0.8;background-color:aliceblue;color:dimgray;}";
+    `;
+    this.executeJavaScript(cmd, this.win.webContents)
+    .then(result => result);
+  }
+
+  promise() {
+    const promise = {};
+    promise.instance = new Promise((resolve, reject) => {
+      promise.resolve = resolve;
+      promise.reject = reject;
+    });
+    return promise;
+  }
+
+  executeJavaScript(cmd, webContents) {
+    const promise = this.promise();
+    webContents.executeJavaScript(cmd, promise.resolve);
+    return promise.instance;
+  }
+
+  light(webContents) {
+    const cmd = `
+    document.head.appendChild(document.createElement("style"))
+    .innerText = "section *, .app-columns-container, .column-header{background-color:white;color:black;}";
+    `;
+    this.executeJavaScript(cmd, webContents)
+    .then(result => result);
+  }
+
+  dark(webContents) {
+    const cmd = `
+    document.head.appendChild(document.createElement("style"))
+    .innerText = "section *, .app-columns-container, .column-header{background-color:black;color:white;}";
+    `;
+    this.executeJavaScript(cmd, webContents)
+    .then(result => result);
+  }
+
+  generateMenu() {
+    menuTemplates[1].submenu[0]
+    .click = (item, focusWindow) => focusWindow && this.dark(focusWindow.webContents);
+    menuTemplates[1].submenu[1]
+    .click = (item, focusWindow) => focusWindow && this.light(focusWindow.webContents);
+  }
+
+  setApplicationMenu() {
+    const menus = [...menuTemplates];
+    this.generateMenu();
+    if (env.name !== 'production') {
+      menus.push(devMenuTemplate);
+    }
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
+  }
+
+  createWindow() {
+    this.win = createWindow('main', {
+      width: 1000,
+      height: 1400,
+    });
+    this.win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true,
+      }),
+    );
+    if (env.name === 'development') {
+      this.win.openDevTools();
+    }
+  }
+}
 
 // Save userData in separate folders for each environment.
 // Thanks to this you can use production and development versions of the app
@@ -31,24 +104,9 @@ if (env.name !== 'production') {
 }
 
 app.on('ready', () => {
-  setApplicationMenu();
-
-  const win = createWindow('main', {
-    width: 1000,
-    height: 1400,
-  });
-
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true,
-    }),
-  );
-
-  if (env.name === 'development') {
-    win.openDevTools();
-  }
+  const daemon = new Daemon();
+  daemon.setApplicationMenu();
+  daemon.createWindow();
 });
 
 app.on('window-all-closed', () => {
